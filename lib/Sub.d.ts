fecc15039f083449eb645b2b53f2862b76967f44
gcanti <giulio.canti@gmail.com>
2017-12-19 10:44:59 +0100

    remove lib folder

--- lib/Sub.d.ts
+++
@@ -1,7 +0,0 @@
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/merge';
-import 'rxjs/add/observable/empty';
-import 'rxjs/add/operator/map';
-export declare type Sub<msg> = Observable<msg>;
-export declare function batch<msg>(arr: Array<Sub<msg>>): Sub<msg>;
-export declare const none: Sub<any>;
--- lib/Time.d.ts
+++
@@ -1,7 +0,0 @@
-import 'rxjs/add/observable/interval';
-import 'rxjs/add/operator/map';
-import { Task } from 'fp-ts/lib/Task';
-import { Sub } from './Sub';
-export declare type Time = number;
-export declare function now(): Task<Time>;
-export declare function every<msg>(time: Time, f: (time: Time) => msg): Sub<msg>;
