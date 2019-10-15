b34b4422d54624aa85d1eb532aef7eebefe5c15d
StefanoMagrassi <1291271+StefanoMagrassi@users.noreply.github.com>
2019-10-15 12:57:26 +0200

    add documentation comments for Time module

--- src/Time.ts
+++ src/Time.ts
@@ -1,9 +1,15 @@
+/**
+ * @file Exposes some utilities to work with unix time.
+ * See [Time](https://package.elm-lang.org/packages/elm/time/latest/Time) Elm package.
+ */
+
 import { Task } from 'fp-ts/lib/Task'
 import { interval } from 'rxjs'
 import { map } from 'rxjs/operators'
 import { Sub } from './Sub'
 
 /**
+ * Get the current unix time as a `Task`.
  * @since 0.5.0
  */
 export function now(): Task<number> {
@@ -11,6 +17,7 @@ export function now(): Task<number> {
 }
 
 /**
+ * Get the current unix time periodically.
  * @since 0.5.0
  */
 export function every<Msg>(time: number, f: (time: number) => Msg): Sub<Msg> {
