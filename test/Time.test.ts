f4792fb92b85f9ef63dbb06d65bdac3eb220e9c2
StefanoMagrassi <1291271+StefanoMagrassi@users.noreply.github.com>
2019-10-15 13:05:41 +0200

    tests for Time module

---
+++ test/Time.test.ts
@@ -0,0 +1,57 @@
+import * as assert from 'assert'
+import { take } from 'rxjs/operators'
+import { every, now } from '../src/Time'
+
+interface Msg {
+  type: 'CURRENT_TIME'
+  time: number
+}
+
+// --- Mocks current time in order to have deterministic tests
+const TIME = 1531313966894
+
+beforeAll(() => {
+  global.Date.prototype.getTime = jest.fn(() => TIME)
+})
+
+afterAll(() => {
+  jest.restoreAllMocks()
+})
+// ---
+
+describe('Time', () => {
+  describe('now()', () => {
+    it('should return a Task which resolves to current Date time', () => now()().then(v => assert.strictEqual(v, TIME)))
+  })
+
+  describe('every()', () => {
+    it('should return a Sub which dispatch a Msg every "n" Time', done => {
+      const log: Msg[] = []
+      const toMsg = (time: number): Msg => ({ type: 'CURRENT_TIME', time })
+      const sub$ = every(250, toMsg)
+
+      sub$.pipe(take(3)).subscribe({
+        next: v => log.push(v),
+
+        complete: () => {
+          assert.deepStrictEqual(log, [
+            {
+              type: 'CURRENT_TIME',
+              time: TIME
+            },
+            {
+              type: 'CURRENT_TIME',
+              time: TIME
+            },
+            {
+              type: 'CURRENT_TIME',
+              time: TIME
+            }
+          ])
+
+          done()
+        }
+      })
+    })
+  })
+})
