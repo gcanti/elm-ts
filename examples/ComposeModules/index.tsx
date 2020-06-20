52ac6e3942d66ddd4741f606b485f202f4ab23ca
Maxwell Brown <maxwellbrown1990@gmail.com>
2020-06-20 16:55:23 -0400

    Use programWithFlags in module composition example
    See https://github.com/gcanti/elm-ts/pull/44#discussion_r439968527

--- examples/ComposeModules/index.tsx
+++ examples/ComposeModules/index.tsx
@@ -5,10 +5,10 @@ import * as React from '../../src/React'
 
 import * as App from './App'
 
-const program = process.env.NODE_ENV === 'production' ? React.program : DebugHtml.programWithDebugger
+const program = process.env.NODE_ENV === 'production' ? React.programWithFlags : DebugHtml.programWithDebuggerWithFlags
 
-const main = program(App.init({ prefix: 'String: ' }), App.update, App.view)
+const main = program(App.init, App.update, App.view)
 
-React.run(main, dom => {
+React.run(main({ prefix: 'String: ' }), dom => {
   ReactDOM.render(dom, document.getElementById('root'))
 })
